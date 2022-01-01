bb97c124c0782b5e4db56dfe41b9bcc145a5aff8
Christian Leingang <chrilei2000@gmail.com>
2022-01-01 12:05:11 +0100

    added SplashScreen & WelcomeScreen

--- frontend/ctfl_vertragsmanager/lib/main.dart
+++ frontend/ctfl_vertragsmanager/lib/main.dart
@@ -1,22 +1,31 @@
 import 'package:ctfl_vertragsmanager/constants/Color_Themes.dart';
+import 'package:ctfl_vertragsmanager/pages/onBoarding.dart';
 import 'package:ctfl_vertragsmanager/pages/vertragsdetails.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import 'pages/mainPages.dart';
 import 'pages/vertragHinzufuegen.dart';
 
 void main() {
-  runApp(const Main());
+  runApp(Main());
 }
 
 class Main extends StatefulWidget {
-  const Main({Key? key}) : super(key: key);
   @override
   State<Main> createState() => _MainState();
 }
 
 class _MainState extends State<Main> {
+  bool _isFirstBoot = true;
+
+  @override
+  void initState() {
+    super.initState();
+    _getFirstBoot();
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -35,8 +44,12 @@ class _MainState extends State<Main> {
         colorScheme: ColorScheme.fromSwatch().copyWith(secondary: ColorThemes.primaryColor),
       ),
       darkTheme: ThemeData.dark(),
-      initialRoute: '/main',
+      initialRoute: _isFirstBoot ? '/intro' : '/main',
       routes: {
+        '/intro': (context) {
+          _storeFirstBoot();
+          return OnBoardingPage();
+        },
         '/main': (context) => MainPages(),
         '/vertragsDetails': (context) => VertragsDetailsPage(),
         '/vertragHinzufuegen': (context) => VertragHinzufuegenPage(),
@@ -44,5 +57,18 @@ class _MainState extends State<Main> {
       //TODO: add theme und darkTheme mit ThemeData() (vgl. 7 Best Tips with Flutter 6. )
       home: MainPages(),
     );
+  }
+
+  void _storeFirstBoot() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    setState() {
+      _isFirstBoot = false;
+      prefs.setBool("isFirstBoot", false);
+    }
+  }
+
+  void _getFirstBoot() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    _isFirstBoot = prefs.getBool("isFirstBoot") ?? true;
   }
 }
