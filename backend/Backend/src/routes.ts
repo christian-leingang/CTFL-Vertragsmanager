cab4dc9d1210eb11dc181c76134619580a5e5ace
Tobi <thoffmann2001@yahoo.de>
2022-01-14 16:08:25 +0100

    Final Code V1.0
    Co-authored-by: michael spengler <michael-spengler@users.noreply.github.com>

--- backend/Backend/src/routes.ts
+++ backend/Backend/src/routes.ts
@@ -1,4 +1,5 @@
 import { Express, Request, Response } from 'express';
+import { createLabelHandler, getLabelHandler } from './controller/label.controller';
 import {
   createProductHandler,
   getProductHandler,
@@ -14,27 +15,31 @@ import {
 import { createUserHandler } from './controller/user.controller';
 import requireUser from './middleware/requireUser';
 import validateResource from './middleware/validateResource';
+import { createLabelSchema, getLabelSchema } from './schema/label.schema';
 import {
   createProductSchema,
   deleteProductSchema,
   getProductSchema,
   updateProductSchema,
 } from './schema/product.schema';
-import { getProductUserSchema } from './schema/productUser.schema';
+import { getProductByIDSchema } from './schema/productUser.model';
 import { createSessionSchema } from './schema/session.schema';
 import { createUserSchema } from './schema/user.schema';
 
 function routes(app: Express) {
-  app.post('/api/users', validateResource(createUserSchema), createUserHandler);
-  app.post('/api/sessions', validateResource(createSessionSchema), createUserSessionHandler);
-  app.get('/api/sessions', requireUser, getUserSessionsHandler);
-  app.delete('/api/sessions', requireUser, deleteSessionHandler);
-  app.post('/api/products', [requireUser, validateResource(createProductSchema)], createProductHandler);
-  app.put('/api/products/:productId', [requireUser, validateResource(updateProductSchema)], updateProductHandler);
-  app.get('/api/products/:productId', validateResource(getProductSchema), getProductHandler);
-  app.delete('/api/products/:productId', [requireUser, validateResource(deleteProductSchema)], deleteProductHandler);
-  //app.get("/api/productsUser/:userId", validateResource(getProductUserSchema),getProductByUserIDHandler);
-  app.get('/healthcheck', (req: Request, res: Response) => res.sendStatus(200));
+  app.post('/api/users', validateResource(createUserSchema), createUserHandler); //Registrieren
+  app.post('/api/sessions', validateResource(createSessionSchema), createUserSessionHandler); //Einloggen
+  app.get('/api/sessions', requireUser, getUserSessionsHandler); //Aktive Sessions returnen
+  app.delete('/api/sessions', requireUser, deleteSessionHandler); //Ausloggen
+  app.post('/api/products', [requireUser, validateResource(createProductSchema)], createProductHandler); //Vertrag anlegen
+  app.put('/api/products/:productId', [requireUser, validateResource(updateProductSchema)], updateProductHandler); //Vertrag bearbeiten
+  app.get('/api/products/:productId', validateResource(getProductSchema), getProductHandler); //Vertrag abrufen
+  app.delete('/api/products/:productId', [requireUser, validateResource(deleteProductSchema)], deleteProductHandler); //Vertrag löschen
+  app.post('/api/labels', validateResource(createLabelSchema), createLabelHandler); //Label anlegen
+  app.get('/api/labels', getLabelHandler); //ALLE labels returnen
+  app.get('/api/productsUser/:userId', validateResource(getProductByIDSchema), getProductByUserIDHandler); //ALLE Verträge eines Users returnen
+  app.get('/healthcheck', (req: Request, res: Response) => res.sendStatus(200)); //Check ob Server läuft
+  
 }
 
 export default routes;
