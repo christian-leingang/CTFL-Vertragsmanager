3fa4b793022fa19cfd52935bea158b90a7171e09
Tobi <thoffmann2001@yahoo.de>
2022-01-27 11:25:17 +0100

    delete user + fixes

--- backend/Backend/src/service/user.service.ts
+++ backend/Backend/src/service/user.service.ts
@@ -35,3 +35,7 @@ export async function validatePassword({
 export async function findUser(query: FilterQuery<UserDocument>) {
   return UserModel.findOne(query).lean();
 }
+
+export async function deleteUser(query: FilterQuery<UserDocument>) {
+  return UserModel.deleteOne(query);
+}
