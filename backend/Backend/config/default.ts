ae994f984d530aa5a4f06b84c92caec61d65ca9e
Tobi <thoffmann2001@yahoo.de>
2022-01-27 16:05:57 +0100

    hiding mongo data in .env

--- backend/Backend/config/default.ts
+++ backend/Backend/config/default.ts
@@ -1,6 +1,6 @@
 export default {
   port: 8080,
-  dbUri: "mongodb://localhost:27017/rest-api",
+  dbUri: ``,
   saltWorkFactor: 10,
   accessTokenTtl: "15m",
   refreshTokenTtl: "1y",
--- backend/Backend/src/utils/connect.ts
+++ backend/Backend/src/utils/connect.ts
@@ -4,10 +4,10 @@ import logger from "./logger";
 
 //PW: DBPW123
 async function connect() {
-  //const dbUri = config.get<string>("dbUri");
-  const dbUriDeploy = "mongodb+srv://CTFLDB:DBPW123@cluster0.nopyw.mongodb.net/CTFL?retryWrites=true&w=majority"
+  const dbUri = config.get<string>("dbUri");
+  console.log(dbUri);
   try {
-    await mongoose.connect(dbUriDeploy);
+    await mongoose.connect(dbUri);
     logger.info("DB connected");
   } catch (error) {
     logger.error("Could not connect to db");
