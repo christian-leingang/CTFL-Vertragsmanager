b8b72f71ae935250a7a1a763ea70d74e5a27c7ef
Tobi <thoffmann2001@yahoo.de>
2022-01-27 15:49:17 +0100

    Deploy MongoDB local -> MongoDB Atlas

--- backend/Backend/src/utils/connect.ts
+++ backend/Backend/src/utils/connect.ts
@@ -2,11 +2,12 @@ import mongoose from "mongoose";
 import config from "config";
 import logger from "./logger";
 
+//PW: DBPW123
 async function connect() {
-  const dbUri = config.get<string>("dbUri");
-
+  //const dbUri = config.get<string>("dbUri");
+  const dbUriDeploy = "mongodb+srv://CTFLDB:DBPW123@cluster0.nopyw.mongodb.net/CTFL?retryWrites=true&w=majority"
   try {
-    await mongoose.connect(dbUri);
+    await mongoose.connect(dbUriDeploy);
     logger.info("DB connected");
   } catch (error) {
     logger.error("Could not connect to db");
