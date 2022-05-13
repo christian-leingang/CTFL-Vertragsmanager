6d426bebb7ae58f07a2de7fa13dee50363f68dd0
Levana-Fallboehmer <57496350+Levana-Fallboehmer@users.noreply.github.com>
2022-05-13 15:44:31 +0200

    updates
    Co-authored-by: Christian Leingang <christian-leingang@users.noreply.github.com>

--- frontend/ctfl_vertragsmanager/lib/pages/onBoarding.dart
+++ frontend/ctfl_vertragsmanager/lib/pages/onBoarding.dart
@@ -27,7 +27,7 @@ class _OnBoardingPageState extends State<OnBoardingPage> {
     const pageDecoration = const PageDecoration(
       titleTextStyle: TextStyle(fontSize: 28.0, fontWeight: FontWeight.w700),
       bodyTextStyle: bodyStyle,
-      descriptionPadding: EdgeInsets.fromLTRB(16.0, 0.0, 16.0, 16.0),
+      // descriptionPadding: EdgeInsets.fromLTRB(16.0, 0.0, 16.0, 16.0),
       pageColor: Colors.white,
       imagePadding: EdgeInsets.zero,
     );
@@ -73,7 +73,7 @@ class _OnBoardingPageState extends State<OnBoardingPage> {
       onDone: () => _onIntroEnd(context),
       //onSkip: () => _onIntroEnd(context), // You can override onSkip callback
       showSkipButton: true,
-      skipFlex: 0,
+      // skipFlex: 0,
       nextFlex: 0,
       //rtl: true, // Display as right-to-left
       skip: const Text(
