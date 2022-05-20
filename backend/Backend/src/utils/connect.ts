1cbaa7759d081bb48e3ab681232e637b1ffec425
Christian Leingang <chrilei2000@gmail.com>
2022-05-20 10:16:07 +0200

    check which dbUri used

--- backend/Backend/src/utils/connect.ts
+++ backend/Backend/src/utils/connect.ts
@@ -1,15 +1,16 @@
-import mongoose from "mongoose";
-import config from "config";
-import logger from "./logger";
+import mongoose from 'mongoose';
+import config from 'config';
+import logger from './logger';
 
 async function connect() {
-  const dbUri = config.get<string>("dbUri");
+  const dbUri = config.get<string>('dbUri');
+  console.log(dbUri);
 
   try {
     await mongoose.connect(dbUri);
-    logger.info("DB connected");
+    logger.info('DB connected');
   } catch (error) {
-    logger.error("Could not connect to db");
+    logger.error('Could not connect to db');
     process.exit(1);
   }
 }
