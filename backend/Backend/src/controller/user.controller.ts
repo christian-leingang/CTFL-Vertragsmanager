c29e349e0c195bb2e279231c60acf29517131f77
Tobi <thoffmann2001@yahoo.de>
2022-06-03 15:46:22 +0200

    Vertaege mitlöschen implementation

--- backend/Backend/src/controller/user.controller.ts
+++ backend/Backend/src/controller/user.controller.ts
@@ -1,6 +1,9 @@
 import { Request, Response } from "express";
 import { omit } from "lodash";
+import contractModel from "../models/contract.model";
 import { CreateUserInput, DeleteUserInput } from "../schema/user.schema";
+import { deletecontract } from "../service/contract.service";
+import { getAllcontractsByUserID } from "../service/contractUser.service";
 import { createUser, deleteUser, findUser } from "../service/user.service";
 import logger from "../utils/logger";
 
@@ -24,16 +27,25 @@ export async function deleteUserHandler(
   try {
     const email = req.body.email;
     const password = req.body.password;
+    var userContracts: any[] = [];
     console.log("Übergebene Email:" + email);
     console.log("Übergebenes Passwort:" + password)
     const user = await findUser({email});
+    const userIdAny = user?.id;
+    const userId = String(userIdAny);
     console.log("Passwort in Datenbank:" + user?.password)
     if (!user) {
       return res.sendStatus(404);
     }
     const isValid = await user.comparePassword(password);
     console.log("Gültiges Passwort:" + isValid);
+    userContracts = await getAllcontractsByUserID({userId});
+    console.log(userContracts);
     if(isValid){
+      for(const contract of userContracts){
+        await deletecontract(contract);
+        console.log(contract.contractId);
+      }
       await deleteUser({ email });
       return res.send(user);
     }
--- backend/Backend/src/service/contractUser.service.ts
+++ backend/Backend/src/service/contractUser.service.ts
@@ -2,12 +2,11 @@ import { FilterQuery, QueryOptions, UpdateQuery } from 'mongoose';
 import contractModel, { contractDocument } from '../models/contract.model';
 
 export async function getAllcontractsByUserID(query: FilterQuery<contractDocument>){
-    var contract = {}
     var toprint = query;
-    console.log(toprint);
+    //console.log(toprint);
     try{
-        contract = await contractModel.find(query);
-        return contract;
+        const contracts = await contractModel.find(query);
+        return contracts;
     }catch(e){
         throw e;
     }
