5d4bcef556eeba747177ad8ba0265f3b597ac1d7
Christian Leingang <chrilei2000@gmail.com>
2022-06-03 16:52:35 +0200

    Profil löschen #20 started Frontend Co-authored-by: Levana-Fallboehmer <Levana-Fallboehmer@users.noreply.github.com>

--- frontend/ctfl_vertragsmanager/lib/pages/profil.dart
+++ frontend/ctfl_vertragsmanager/lib/pages/profil.dart
@@ -3,6 +3,7 @@ import 'package:ctfl_vertragsmanager/funktionen/db_functions.dart';
 import 'package:ctfl_vertragsmanager/funktionen/profil_funktionen.dart';
 import 'package:ctfl_vertragsmanager/models/profile.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 class ProfilPage extends StatefulWidget {
   Profile user = Profile(email: "", password: "");
