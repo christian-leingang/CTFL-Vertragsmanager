53a157965212b29d8086301a392fe04fa1d40169
tobiashoffmann <thoffmann2001@yahoo.de>
2022-06-14 16:07:26 +0100

    removed cors

--- backend/Backend/src/app.ts
+++ backend/Backend/src/app.ts
@@ -9,13 +9,13 @@ import deserializeUser from "./middleware/deserializeUser";
 import { sendMail } from "./utils/mailer";
 
 const port = config.get<number>("port");
-const cors = require('cors');
+
 
 const app = express();
 
 app.use(express.json());
 app.use(deserializeUser);
-app.use(cors());
+
 //sendMail();
 app.listen(port, async () => {
   logger.info(`App is running at http://localhost:${port}`);
